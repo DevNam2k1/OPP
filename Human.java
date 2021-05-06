5168765d70a8ff90790ad93904092ae09fe5c249
DevNam2k1 <dinhnamsaker@gmail.com>
2021-05-07 00:37:32 +0700

    Create a new OPP

---
+++ Human.java
@@ -0,0 +1,12 @@
+public class Human {
+   String name;
+   int age;
+   int weight;
+   
+   Human(String name,int age,int weight){
+	   
+	   this.name = name;
+	   this.age = age;
+	   this.weight = weight;
+   }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+   
+	 public static void main(String[] args) {
+		 
+		 
+		 Human human = new Human("Kelvin",65,78);
+		 
+		 System.out.println(human.name);
+	 }
+}
